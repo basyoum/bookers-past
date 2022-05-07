02d3d5b39bf4ec9155aa6b1421fcecb1cf99f6f8
EC2 Default User <ec2-user@ip-172-31-20-241.ec2.internal>
2022-05-07 13:33:48 +0000

    [Finish]新規投稿フォームをindexページ内に置く

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,4 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
-
   def create
     @book = Book.new(book_params)
     if @book.save
@@ -15,6 +11,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
