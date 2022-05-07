05b92416c3d59eaa967873c275982a3b907aa491
EC2 Default User <ec2-user@ip-172-31-20-241.ec2.internal>
2022-05-07 02:03:11 +0000

    [Finish]編集機能を作ろう

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   post 'books' => 'books#create'
   get 'books' => 'books#index'
   get 'books/show'
-  get 'books/edit'
+
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+
   get '/top' => 'homes#top'
   get 'books/:id' => 'books#show',as: 'book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
