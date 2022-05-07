6fbdc217dfb17ba7eb020ce7bbcd00c62b66a157
EC2 Default User <ec2-user@ip-172-31-20-241.ec2.internal>
2022-05-07 02:18:50 +0000

    [Finish]削除機能を作ろう(resourcesメソッドはまだ)

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
 
   get '/top' => 'homes#top'
   get 'books/:id' => 'books#show',as: 'book'
+
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
